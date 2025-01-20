8153951381a10e936e382ef2e76e514910024b55
Kemal Aras Tosunlar <tosunlarkemal@gmail.com>
2025-01-20 14:45:55 +0300

    Update ProductContext.jsx

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -11,9 +11,9 @@ export const ProductProvider = ({ children }) => {
     // Sample product data
     const sampleProducts = [
       { id: 1, title: 'The Art Of War', price: 10.11, image: 'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg' },
-      { id: 2, title: '1984', price: 8.99, image: 'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg' },
-      { id: 3, title: 'To Kill a Mockingbird', price: 12.99, image: 'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg' },
-      { id: 4, title: 'The Great Gatsby', price: 14.99, image: 'https://images-na.ssl-images-amazon.com/images/I/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg' },
+      { id: 2, title: '1984', price: 8.99, image: 'https://images-na.ssl-images-amazon.com/images/I/51Jj12iMZnL._SX331_BO1,204,203,200_.jpg' },
+      { id: 3, title: 'To Kill a Mockingbird', price: 12.99, image: 'https://images-na.ssl-images-amazon.com/images/I/512-B-1yXuL._SX331_BO1,204,203,200_.jpg' },
+      { id: 4, title: 'The Great Gatsby', price: 14.99, image: 'https://images-na.ssl-images-amazon.com/images/I/61uUPXbhMxL._SX397_BO1,204,203,200_.jpg' },
     ];
     setProducts(sampleProducts);
   };
